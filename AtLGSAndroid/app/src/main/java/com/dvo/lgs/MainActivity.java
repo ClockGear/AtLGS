3820ab2a50ebbff4d1a7128ce137c8ad65f8a33f
Dennis van Opstal <denstal1@home.nl>
2017-12-01 17:14:24 +0100

    Added some fragments and stuff

--- AtLGSAndroid/app/src/main/java/com/dvo/lgs/MainActivity.java
+++ AtLGSAndroid/app/src/main/java/com/dvo/lgs/MainActivity.java
@@ -1,13 +1,40 @@
 package com.dvo.lgs;
 
+import android.os.Build;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager pager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pager = findViewById(R.id.pager);
+    }
+
+    public void menuItemClick(View view) {
+        switch (Integer.valueOf((String)view.getTag())) {
+            case 1: {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    pager.setBackgroundColor(getColor(R.color.primaryColor));
+                } else {
+                    pager.setBackgroundColor(getResources().getColor(R.color.primaryColor));
+                }
+                break;
+            }
+            case 2: {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    pager.setBackgroundColor(getColor(R.color.primaryDarkColor));
+                } else {
+                    pager.setBackgroundColor(getResources().getColor(R.color.primaryDarkColor));
+                }
+                break;
+            }
+        }
     }
 }
---
+++ AtLGSAndroid/app/src/main/java/com/dvo/lgs/fragments/PresenceFragment.java
@@ -0,0 +1,10 @@
+package com.dvo.lgs.fragments;
+
+import android.app.Fragment;
+
+/**
+ * Created by Dennis van Opstal on 1-12-2017.
+ */
+
+public class PresenceFragment extends Fragment {
+}
