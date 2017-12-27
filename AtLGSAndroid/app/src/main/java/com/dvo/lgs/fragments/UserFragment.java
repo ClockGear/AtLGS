67a70487f23adf2d6872986ef08ee40750ea4d4f
Dennis van Opstal <denstal1@home.nl>
2017-12-27 18:19:21 +0100

    Added some calls to the UserController

--- AtLGSAndroid/app/src/main/java/com/dvo/lgs/fragments/UserFragment.java
+++ AtLGSAndroid/app/src/main/java/com/dvo/lgs/fragments/UserFragment.java
@@ -5,7 +5,6 @@ import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Vibrator;
 import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
@@ -13,17 +12,32 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.dvo.lgs.R;
 import com.dvo.lgs.adapters.UserAdapter;
 import com.dvo.lgs.domain.User;
+import com.dvo.lgs.enums.Role;
 import com.dvo.lgs.util.OnItemLongClickListener;
+import com.dvo.lgs.volley.AppController;
+import com.dvo.lgs.volley.BetterStringRequest;
+import com.google.gson.Gson;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Dennis van Opstal on 8-12-2017.
@@ -32,15 +46,14 @@ import java.util.List;
 public class UserFragment extends Fragment {
 
     private RecyclerView recyclerView;
-    private FloatingActionButton fabAddUser;
     private UserAdapter adapter;
     private List<User> users;
-    private AlertDialog alertDialog;
     private SwipeRefreshLayout swipeRefreshLayout;
     private static final String VOLLEY_TAG = "VOLLEY - USERS";
     private SharedPreferences sharedPref;
     private String token;
     private boolean isAdmin;
+    private long ownUserId;
 
     public static UserFragment newInstance() {
         UserFragment userFragment = new UserFragment();
@@ -60,8 +73,8 @@ public class UserFragment extends Fragment {
         adapter = new UserAdapter(users, new OnItemLongClickListener() {
             @Override
             public void onItemLongClick(int position) {
-                if (isAdmin) {
-                    final User user = users.get(position);
+                final User user = users.get(position);
+                if (isAdmin && user.getId() != ownUserId) {
                     Vibrator vibrator = (Vibrator) getContext().getSystemService(Context.VIBRATOR_SERVICE);
                     if (vibrator != null) {
                         vibrator.vibrate(50);
@@ -72,7 +85,7 @@ public class UserFragment extends Fragment {
                     builder.setPositiveButton(getString(R.string.yes), new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialogInterface, int i) {
-                            //TODO DELETE USER
+                            deleteUser(user.getId());
                             dialogInterface.dismiss();
                         }
                     });
@@ -91,7 +104,7 @@ public class UserFragment extends Fragment {
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
-                //TODO GET ALL USERS
+                getAllUsers();
             }
         });
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getContext());
@@ -100,6 +113,175 @@ public class UserFragment extends Fragment {
         recyclerView.setAdapter(adapter);
         sharedPref = getContext().getSharedPreferences(getString(R.string.def_pref), Context.MODE_PRIVATE);
         token = sharedPref.getString(getString(R.string.login_token), "");
-        //TODO GET ALL USERS AND CHECK ROLE
+        getAllUsers();
+        roleCheck();
+    }
+
+    private void getAllUsers() {
+        String requestTag = "get_all_users_request";
+        String url = getString(R.string.api_url) + "/user";
+        BetterStringRequest validationRequest = new BetterStringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d(VOLLEY_TAG, response);
+                try {
+                    JSONObject json = new JSONObject(response);
+                    JSONArray userJsonArray = json.getJSONArray("object");
+                    users.clear();
+                    for (int i = 0; i < userJsonArray.length(); i++) {
+                        JSONObject lgsJsonObject = (JSONObject) userJsonArray.get(i);
+                        User user = new Gson().fromJson(lgsJsonObject.toString(), User.class);
+                        users.add(user);
+                    }
+                    swipeRefreshLayout.setRefreshing(false);
+                    adapter.notifyDataSetChanged();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                String jsonString = error.getMessage();
+                if (jsonString == null || jsonString.isEmpty()) {
+                    createNewErrorDialog(R.string.err_something_wrong);
+                } else {
+                    Log.e(VOLLEY_TAG, jsonString);
+                    try {
+                        JSONObject json = new JSONObject(jsonString);
+                        String message = json.getString("message");
+                        switch (message) {
+                            default:
+                                createNewErrorDialog(R.string.err_something_wrong);
+                                break;
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        createNewErrorDialog(R.string.err_something_wrong);
+                    }
+                }
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String,String> headers = new HashMap<>();
+                headers.put("Authorization","Bearer " + token);
+                return headers;
+            }
+        };
+        AppController.getInstance().addToRequestQueue(validationRequest, requestTag);
+    }
+
+    private void roleCheck() {
+        final String requestTag = "role_check_request";
+        String url = getString(R.string.api_url) + "/user/validation";
+        BetterStringRequest validationRequest = new BetterStringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d(VOLLEY_TAG, response);
+                try {
+                    JSONObject json = new JSONObject(response);
+                    final JSONObject jsonUser = json.getJSONObject("object");
+                    Role role = Role.valueOf(jsonUser.getString("role"));
+                    ownUserId = jsonUser.getLong("id");
+                    if (role.equals(Role.ADMIN)) {
+                        getActivity().runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                isAdmin = true;
+                            }
+                        });
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                String jsonString = error.getMessage();
+                if (jsonString == null || jsonString.isEmpty()) {
+                    createNewErrorDialog(R.string.err_something_wrong);
+                } else {
+                    Log.e(VOLLEY_TAG, jsonString);
+                    try {
+                        JSONObject json = new JSONObject(jsonString);
+                        String message = json.getString("message");
+                        switch (message) {
+                            default:
+                                createNewErrorDialog(R.string.err_something_wrong);
+                                break;
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        createNewErrorDialog(R.string.err_something_wrong);
+                    }
+                }
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String,String> headers = new HashMap<>();
+                headers.put("Authorization","Bearer " + token);
+                return headers;
+            }
+        };
+        AppController.getInstance().addToRequestQueue(validationRequest, requestTag);
+    }
+
+    private void deleteUser(long id) {
+        String requestTag = "delete_user_request";
+        String url = getString(R.string.api_url) + "/user?id=" + id;
+        BetterStringRequest validationRequest = new BetterStringRequest(Request.Method.DELETE, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d(VOLLEY_TAG, response);
+                getAllUsers();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                String jsonString = error.getMessage();
+                if (jsonString == null || jsonString.isEmpty()) {
+                    createNewErrorDialog(R.string.err_something_wrong);
+                } else {
+                    Log.e(VOLLEY_TAG, jsonString);
+                    try {
+                        JSONObject json = new JSONObject(jsonString);
+                        String message = json.getString("message");
+                        switch (message) {
+                            case "Something went wrong while deleting the User.":
+                                createNewErrorDialog(R.string.err_something_wrong);
+                                break;
+                            case "User with the given id doesn't exist.":
+                                createNewErrorDialog(R.string.err_user_id_exist);
+                                break;
+                            default:
+                                createNewErrorDialog(R.string.err_something_wrong);
+                                break;
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        createNewErrorDialog(R.string.err_something_wrong);
+                    }
+                }
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String,String> headers = new HashMap<>();
+                headers.put("Authorization","Bearer " + token);
+                return headers;
+            }
+        };
+        AppController.getInstance().addToRequestQueue(validationRequest, requestTag);
+    }
+
+    private void createNewErrorDialog(int message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle(R.string.error);
+        builder.setMessage(message);
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
---
+++ AtLGSAndroid/app/src/main/java/com/dvo/lgs/util/OnItemClickListener.java
@@ -0,0 +1,9 @@
+package com.dvo.lgs.util;
+
+/**
+ * Created by Dennis van Opstal on 27-12-2017.
+ */
+
+public interface OnItemClickListener {
+    void onItemClick(int position);
+}
